7431ef985d04e577ddd73379648d50574bcad8e9
emil <201811346@post.au.dk>
2020-06-12 21:52:21 +0200

    pcp solver virker

--- pcp.py
+++ pcp.py
@@ -1,9 +1,22 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
 # read the input
 # input is on the form: i d1 d2 .. dn
 # where i is the number of iterations and n is the number of pairs
 
-# inp = input().split()
-inp = '6 1#101 10#00 011#11'.split()
+# example inputs
+# 6 1#101 10#00 011#11
+# 70 001#0 01#011 01#101 10#001
+
+print("Please specify the number of iterations you want to make and list the dominos to be used.")
+print("Top and bottom rows are divieded by a #.")
+print("Example: 6 1#101 10#00 011#11")
+inp = input("Input: ").split()
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+# inp = '6 1#101 10#00 011#11'.split()
 
 # k = number of iterations
 k = int(inp[0])
@@ -16,7 +29,8 @@ for i in range(1, len(inp)):
     domino = inp[i].split("#")
     dominos.append(domino)
 
-print("Running pcp with input: " + str(dominos))
+print("Running pcp with input dominos: " + str(dominos))
+print("")
 
 # n = number of dominos
 n = int(len(dominos))
@@ -24,62 +38,75 @@ n = int(len(dominos))
 
 def pcp():
     # (1) create a solutions list
-    print("Number of dominos: " + str(n))
     solutions = []
     for i in range(0, n):
-        print(dominos[i])
+        print(f"Domino 1: {dominos[i]}")
         xi = dominos[i][0]
         yi = dominos[i][1]
         if (xi == yi):
             return dominos[i]
         if (len(xi) < len(yi)):
             if (yi[:len(xi)] == xi):
-                solutions.append(dominos[i])
+                solutions.append([dominos[i]])
         if (len(yi) < len(xi)):
             if (xi[:len(yi)] == yi):
-                solutions.append(dominos[i])
+                solutions.append([dominos[i]])
 
-    print("Solutions after (1): " + str(solutions))
+    # print("Dominos that can be used first: " + str(solutions))
 
-    print("Number of iterations: " + str(k))
     # (2) loop through each iteration k
     for i in range(k):
         new_solutions = []
         for solution in solutions:
             for j in range(0, n):
-                new_solution = [solution]
+                new_solution = solution.copy()
                 xj = dominos[j][0]
                 yj = dominos[j][1]
                 new_solution.append([xj, yj])
-                print(new_solution)
                 xlist = ""
                 ylist = ""
                 for domino in new_solution:
-                    xlist = xlist + domino[0]
-                    ylist = ylist + domino[1]
+                    xlist += domino[0]
+                    ylist += domino[1]
                 if(len(xlist) == len(ylist)):
-                    new_solutions = new_solutions + new_solution
+                    if (xlist == ylist):
+                        new_solutions += [new_solution]
                 if (len(xlist) < len(ylist)):
                     if (ylist[:len(xlist)] == xlist):
-                        new_solutions = new_solutions + new_solution
+                        new_solutions += [new_solution]
                 if (len(ylist) < len(xlist)):
                     if (xlist[:len(ylist)] == ylist):
-                        new_solutions = new_solutions + new_solution
-        print("New solutions: " + str(new_solutions))
+                        new_solutions += [new_solution]
         if (new_solutions == []):
             return []
         xlength = 0
         ylength = 0
         for solution in new_solutions:
-            print("Solution: " + str(solution))
-            xlength = xlength + len(solution[0])
-            ylength = ylength + len(solution[1])
-            print("xlength: " + str(xlength) + " ylength: " + str(ylength))
+            xlength = 0
+            ylength = 0
+            for domino in solution:
+                xlength += len(domino[0])
+                ylength += len(domino[1])
             if (xlength == ylength):
                 return solution
-        solutions = new_solutions
-    return solutions
+        solutions = new_solutions.copy()
+    return []
 
-print("Solution to this pcp: " + str(pcp()))
+res = pcp()
 
-# example input: 6 1#101 10#00 011#11
+print("")
+print("--------------------------------------------------------------------------------------")
+
+if (res != []):
+    print("Solution to this pcp: " + str(res))
+    xlist = ""
+    ylist = ""
+    for domino in res:
+        xlist += domino[0]
+        ylist += domino[1]
+    print(f"{xlist} <--- top row")
+    print(f"{ylist} <--- bottom row")
+else:
+    print("Could not find a solution for this pcp. Try with more iterations...")
+
+print("--------------------------------------------------------------------------------------")
